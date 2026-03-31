0cc2e54f0842a24e7bb8b2889bf134345390daa1
Mouad Gourita <mouad.gourita@gmail.com>
2026-03-31 16:23:56 +0100

    feat: integrate Vue.js, add Login and Products components, and define associated routes and views

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('login', [AuthController::class, 'index']);
+Route::get('register', [AuthController::class, 'indexRegister'])->middleware('guest');
+
+Route::get('products', [ProductController::class, 'showProducts']);
